5352053dc55ecf2b5c378844f323221fd8aca597
hinkleydev <azz@hinkley.dev>
2024-11-08 10:41:34 +0000

    Create get all shows function

--- routes/Shows.js
+++ routes/Shows.js
@@ -1,9 +1,20 @@
 const { Router } = require("express");
+const { Show } = require("../models/index.js");
 
 const router = Router();
 
-router.get("/", function(req, res) {
-    res.send("Shows root");
+// --- CREATE operations ---
+
+// --- READ operations ---
+
+// List all shows
+router.get("/", async function(req, res) {
+    const shows = await Show.findAll();
+    res.json(shows);
 })
+
+// --- UPDATE operations ---
+
+// -- DELETE operations ---
 
 module.exports = { shows: router }
