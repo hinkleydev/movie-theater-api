9ff0fc928fc2a1c9864d2bf4e1d56a0e389d004d
hinkleydev <azz@hinkley.dev>
2024-11-08 10:03:43 +0000

    Create get all users function

--- routes/Users.js
+++ routes/Users.js
@@ -1,9 +1,19 @@
 const { Router } = require("express");
-
+const { User } = require("../models/index.js");
 const router = Router();
 
-router.get("/", function(req, res) {
-    res.send("Users root");
+// --- CREATE operations ---
+
+// --- READ operations ---
+
+// Get all users - /users
+router.get("/", async function(req, res) {
+    const users = await User.findAll();
+    res.json(users);
 })
+
+// --- UPDATE operations ---
+
+// --- DELETE operations
 
 module.exports = { users: router }
