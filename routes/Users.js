ce7b8a23e4b7025db6ad61c901c90c58d6a9676d
hinkleydev <azz@hinkley.dev>
2024-11-08 14:07:58 +0000

    Add validators to user routes

--- routes/Users.js
+++ routes/Users.js
@@ -1,6 +1,19 @@
 const { Router } = require("express");
 const { User, Show } = require("../models/index.js");
+const { check, validationResult } = require("express-validator");
 const router = Router();
+
+// These functions will check the database to see if the user ID is valid
+async function checkUserID(value) {
+    const user = await User.findByPk(value);
+    if (user == null) {
+        // Not found
+        throw "User not found";
+    }
+}
+
+const isValidUserID = value => check("id").custom(checkUserID)
+
 
 // --- CREATE operations ---
 
@@ -13,13 +26,27 @@ router.get("/", async function(req, res) {
 })
 
 // Get a user - /users/:id
-router.get("/:id", async function(req, res) {
+router.get("/:id", 
+    isValidUserID(), 
+    async function(req, res) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.status(404).json({ errors : errors.array() });
+        return;
+    }
     const user = await User.findByPk(req.params.id);
     res.json(user)
 })
 
 // Get shows a user has watched - /users/:id/shows
-router.get("/:id/shows", async function(req, res) {
+router.get("/:id/shows", 
+    isValidUserID(),
+    async function(req, res) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.status(404).json({ errors : errors.array() });
+        return;
+    }
     const user = await User.findByPk(req.params.id, {include: Show})
     res.json(user.shows); // It doesn't make sense to include the user again here IMO
 })
@@ -27,9 +54,20 @@ router.get("/:id/shows", async function(req, res) {
 // --- UPDATE operations ---
 
 // Add a record of a watched show to a users account - /users/:id/shows/:show
-router.put("/:id/shows/:show", async function(req, res) {
+router.put("/:id/shows/:show", 
+    isValidUserID(), 
+    async function(req, res) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.status(404).json({ errors : errors.array() });
+        return;
+    }
     let user = await User.findByPk(req.params.id);
     const show = await Show.findByPk(req.params.show);
+    if (show == null) {
+        res.status(404).json({ errors: "Show not found"});
+        return;
+    }
     await user.addShow(show);
     user = await User.findByPk(req.params.id, {include: Show});
     res.status(201).json(user.shows);
